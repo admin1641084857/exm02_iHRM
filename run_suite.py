0134f902c503b0c784907694584185be7763e792
admin1641084857 <56428864+admin1641084857@users.noreply.github.com>
2019-10-17 16:27:07 +0800

    Update run_suite.py

--- run_suite.py
+++ run_suite.py
@@ -16,5 +16,8 @@ suite.addTest(TestEmployee("test_emp_get"))
 suite.addTest(TestEmployee("test_emp_delete"))
 
 # 执行套件对象
-runner = unittest.TextTestRunner()
-runner.run(suite)
+# runner = unittest.TextTestRunner()
+# runner.run(suite)
+with open(app.PRO_PATH + "/report/repter.html", "wb") as f:
+    runner = HTMLTestRunner(f, title="hello", description="v1.0")
+    runner.run(suite)
